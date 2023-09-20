ecc5d7d06aa592d545e4f7eed40913839344ee64
karani-ken <academice79@gmail.com>
2023-09-20 10:13:16 +0300

    first commit

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -1,6 +1,9 @@
+using Blazored.LocalStorage;
 using Frontend;
 using Frontend.Services;
+using Frontend.Services.AuthProvider;
 using Frontend.Services.Interfaces;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,7 +12,17 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//register localstorage
+builder.Services.AddBlazoredLocalStorage();
+
+//services
 builder.Services.AddScoped<IAuthInterface, AuthService>();
+builder.Services.AddScoped<IPostInterface, PostService>();
+
+//configurations for authorization
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProviderService>();
 
 
 await builder.Build().RunAsync();
